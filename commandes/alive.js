9619cf722baeb21bd320f7127909642dcbbfe231
DARK SASIYA <164819368+feenixmd@users.noreply.github.com>
2024-05-06 11:12:59 +0530

    🍀𝐅𝐄𝐄𝐍𝐈𝐗-𝐕5🍁

---
+++ commandes/alive.js
@@ -0,0 +1,94 @@
+const { zokou } = require('../framework/zokou');
+const {addOrUpdateDataInAlive , getDataFromAlive} = require('../bdd/alive')
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+zokou(
+    {
+        nomCom : 'alive',
+        categorie : 'General'
+        
+    },async (dest,zk,commandeOptions) => {
+
+ const {ms , arg, repondre,superUser} = commandeOptions;
+
+ const data = await getDataFromAlive();
+
+ if (!arg || !arg[0] || arg.join('') === '') {
+
+    if(data) {
+       
+        const {message , lien} = data;
+
+
+        var mode = "public";
+        if ((s.MODE).toLocaleLowerCase() != "yes") {
+            mode = "private";
+        }
+      
+    
+     
+    moment.tz.setDefault('EAT');
+
+// Créer une date et une heure en EAT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+    const alivemsg = `
+*Owner* : ${s.OWNER_NAME}
+*Mode* : ${mode}
+*Date* : ${date}
+*Time* : ${temps}
+
+ ${message}
+ 
+ 
+ *FEENIX-MD-V5 by FEENIX MD is alive*`
+
+ if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Checking for .jpeg or .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(alivemsg);
+    
+}
+
+    } else {
+        if(!superUser) { repondre("there is no alive for this bot") ; return};
+
+      await   repondre("⚔ 𝐅𝐄𝐄𝐍𝐈𝐗 𝐌𝐃 𝐕5 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 𝗢𝗪𝗡𝗘𝗥 𝗙𝗘𝗘𝗡𝗜𝗫 🍁");
+         repondre(" *Support feenix md ! dark sasiya")
+     }
+ } else {
+
+    if(!superUser) { repondre ("Only the owner can  modify the alive") ; return};
+
+  
+    const texte = arg.join(' ').split(';')[0];
+    const tlien = arg.join(' ').split(';')[1]; 
+
+
+    
+await addOrUpdateDataInAlive(texte , tlien)
+
+repondre('message alive refresh successfully')
+
+}
+    });
